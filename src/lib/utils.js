323dc8f05d0b87cabe30127ddb8f859c2ad2bfe2
SangemAbhinay <sangemabhinay18@gmail.com>
2025-07-19 09:01:19 +0530

    port

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly: true,
-        sameSite: "None",                // ✅ Cross-origin safe
-        secure: process.env.NODE_ENV !== "development" // ✅ required for SameSite: None
-    });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    sameSite: "None",                // 🔥 Required for cross-origin
+    secure: true                     // 🔥 Must be true for SameSite: None
+  });
 
-    return token;
+  return token;
 };
